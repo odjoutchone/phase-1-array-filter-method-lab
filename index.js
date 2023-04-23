31694908aa2732026b41347acfa05ed3f40898ca
Odjoutchone Sylvestre Tokou <odjoutchone@yahoo.fr>
2023-04-23 15:24:45 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, string){
+const result = array.filter(word => word.toUpperCase() === string.toUpperCase())
+    return result;
+}
+
+function fuzzyMatch(array, string){
+const result = array.filter(word => word.slice(0,2)===string.slice(0,2))
+    return result;
+}
+
+function matchName(array,string){
+const result = array.filter(({name}) => name === string);
+    return result;
+}
+
